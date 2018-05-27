3c4c1e170c2dd672239c8c61de45d6156ae7e118
ashu <Utkarsh.Bisht7@gmail.com>
2018-05-27 15:37:42 +0530

    File upload functionality. Isolated file read functionality

--- routes/registration.js
+++ routes/registration.js
@@ -4,6 +4,8 @@ const express = require('express');
 const router = express.Router();
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
+const Busboy = require('busboy');
+const fs = require('fs');
 
 const validator = require('../validators/registrationValidator');
 const persistUser = require('../persistence/user_dao');
@@ -17,31 +19,74 @@ router.get('/', (req, res)=>{
 router.post('/user', (req, res)=>{
     const result = validator.validate(req.body);
     if(result.error){
-        console.log('Validation error: ', result.error);
-        return res.status(400).send(`Invalid request send to register the user:  ${req.body}`);
+        console.log('Validation error: ', result.error.message);
+        return res.status(400).send(`Invalid request send to register the user:  ${req.body.username}`);
     }
     console.log('Validation successful');
     console.log('Logger body: ', req.body);
     const user = userModel.buildUser(req.body);
-
-    saveUser(req, res, user);
+    console.log("User prepared: ", user);
+    return saveUser(res, user); //Validate when to use then or return
 });
 
+//Can be replicated by postman client
+router.get('/user/upload', function (req, res) {
+    res.send('<html><head></head><body>\
+               <form method="POST" enctype="multipart/form-data">\
+                <input type="text" name="textfield"><br />\
+                <input type="file" name="filefield"><br />\
+                <input type="submit">\
+              </form>\
+            </body></html>');
+  res.end();
+});
 
-async function saveUser(req, res, user){
+router.post('/user/upload', (req, res)=>{
+    var busboy = new Busboy({headers: req.headers});
+    console.log('Busboy instantiated: ');
+    busboy.on('file', fileUploadListener);
+    busboy.on('finish', ()=>{
+        console.log('File uploaded successfully');
+        res.writeHead(200, { 'Connection': 'close' });
+        res.end("File write complete");
+    });
+    
+    return req.pipe(busboy);
+
+    // readFileAndSendToSave(req, res, busboy); //Call this function either here or in event
+    
+});
+
+function fileUploadListener(fieldname, file, filename, encoding, mimetype){
+    var saveTo = "F:\\"+ filename; // To be present in config file
+    console.log('Uploading to ', saveTo);
+    file.pipe(fs.createWriteStream(saveTo));
+}
+
+async function readFileAndSendToSave(req, res, busboy){
+    await req.pipe(busboy);
+    var user = {};
+    await fs.readFile("F:\\fil.txt", 'utf8', function(err, content){ //Ti be loaded from config file
+        console.log(content);
+        user = content;
+    });
+    return saveUser(res, user); //Validate then vs return
+}
+
+async function saveUser(res, user){
     await hash.hashPassword(user.password).then((result)=>{
         console.log('Result: '+ result);
         user.password = result;
     }).catch(err => {
-        console.log('Error in hashing the password');
-        res.status(500).send('Internal error occurred while saving the user');
+        console.log('Error in hashing the password. ', err.message);
+        return res.status(500).send('Internal error occurred while saving the user');
     });
 
     persistUser.createUser(user).then(
         ()=>{
                 console.log('Success received from mongo save');
                 // return res.status(200).send('Successfully saved user '+ _.pick(user,['username', 'email', 'firstName',
-                //                             'middleName', 'lastName']));
+                //                             'middleName', 'lastName'])); //Check why lodash pick is not returning text but unparsed object
                 return res.status(200).send('Successfully saved user '+ user);
         }
     ).catch(
--- utility/hash.js
+++ utility/hash.js
@@ -2,6 +2,7 @@
 const bcrypt = require('bcrypt');
 
 async function hashPassword(password){
+    console.log('Request received to hash password: ', password);
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
     console.log('Hashed password ', hashPassword);
