6c7f545fc1e5247ba6d290473f8315ec9da8dade
ashu <Utkarsh.Bisht7@gmail.com>
2018-08-15 16:14:24 +0530

    Added test service for benchmark

--- routes/benchmark.js
+++ routes/benchmark.js
@@ -12,12 +12,21 @@ router.get('/getDataQuick', (req, res) =>{
     res.send("Here's some quick data without async");
 });
 
-router.get('/getDataAfterTwo', (req, res)=>{
+router.get('/getDataAfterTen', (req, res)=>{
 	return getDataAfterTimeOut(res);
 })
 
 router.get('/doLongOprnAsync', async (req,res)=>{
-	return await longAsyncOprn(res);
+	return longAsyncOprn(res);
+})
+
+router.get('/doLongOprnAsync2', async (req,res)=>{
+	const time = await longAsyncOprn2(res);
+	res.send("Operation completed in "+ time + " millis");
+})
+
+router.get('/doLongOprnAsyncCallback', async (req,res)=>{
+	return longAsyncOprnCallb(res);
 })
 
 router.get('/corruptOprn', (req,res)=>{
@@ -25,29 +34,49 @@ router.get('/corruptOprn', (req,res)=>{
 	const stream = fs.createReadStream('does-not-exist.txt');
 })
 
+
+
+
+
 async function longAsyncOprn(res){
 	const date = Date.now();
-	await longLoop();
+	longLoop();
 	const time = Date.now() - date;
-	res.send("Operation completed in ", time, " millis");
+	return res.send(`Operation comepleted in ${time} millis`);
+}
+
+async function longAsyncOprn2(res){
+	return new Promise((resolve, reject)=>{
+		const date = Date.now();
+		for(var i =0; i<10000000000; i++){
+		}
+		const time = Date.now() - date;
+		resolve(time);
+	})
+}
+
+async function longAsyncOprnCallb(res){
+	const date = Date.now();
+	setTimeout(()=>{
+		console.log("Data received");
+		const time = Date.now() - date;
+		res.send(`Operation completed in ${time/1000} seconds`);
+	},0);
 }
 
 async function longLoop(){
-	for(var i =0; i<100000000; i++){
-		for(var j=0; j<100000; j++){
-
-		}
+	for(var i =0; i<10000000000; i++){
 	}
 }
 
 async function getDataAfterTimeOut(res){
 	const date = Date.now();
-	await setTimeout(() => {
-		console.log("Doing some operation");
-	}, 2000);
-	console.log("Data received");
-	const time = Date.now() - date;
-	res.send("Operation completed in ", time, " millis");
+	await setTimeout(async() => {
+		await console.log("Doing some operation");
+		console.log("Data received");
+		const time = Date.now() - date;
+		res.send(`Operation completed in ${time/1000} seconds`)
+	}, 10000);
 }
 
 
